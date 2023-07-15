9039d5e7fc1af210bd437548488ebf1b4b76d6d8
JHarringtonDev <jasanth97@gmail.com>
2023-07-15 13:06:21 -0700

    fixed mongoDB issues

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,17 @@
 import {checkDexNumber, answer} from "./pokemonNum.js"
 
+const db = ""
+
+let currentScore = 0
+// let pullScore = document.querySelector("#highDisplay").innerText
+// let pbScore = parseFloat(pullScore.replace('Personal Best: ', ""))
+// const loadScore = pbScore
+let selectedMon 
+let rolled = []
+
 document.querySelector(".show").addEventListener("click", removeClass)
 document.querySelector(".getMon").addEventListener("click", chooseMon)
+// document.querySelector("#highDisplay").innerText = `Personal Best: ${pbScore}`
 document.querySelector("#userAnswer").addEventListener("keypress", function(event){
     if (event.key === "Enter"){
         if(document.querySelector(".mon").classList.contains("hide")){
@@ -12,22 +22,22 @@ document.querySelector("#userAnswer").addEventListener("keypress", function(even
     }
 });
 
-let selectedMon 
-// let answer 
-let rolled = []
-
 function removeClass() {
     checkDexNumber(selectedMon)
     let userChoice = document.querySelector("#userAnswer").value
     if(userChoice.toLowerCase() == answer.toLowerCase()){
         document.querySelector(".resultPrompt").innerText = `That's correct! It's ${answer}!`
+        currentScore++
+        // if (pbScore < currentScore){
+        //     pbScore = currentScore
+        // }
+        document.querySelector("#currentDisplay").innerText = `Current Score: ${currentScore}`
+        // document.querySelector("#highDisplay").innerText = `Personal Best: ${pbScore}`
     }else{
     document.querySelector(".resultPrompt").innerText = `Sorry that's incorrect. It's ${answer}!`
     }
     document.querySelector(".mon").classList.remove("hide")
 }
-
-
 
 function chooseMon() {
     document.querySelector(".resultPrompt").innerText = `Who's That Pokémon?`
